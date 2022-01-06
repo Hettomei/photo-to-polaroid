09fe5b86f34152aa36c8a25163562291ac4e693d
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2022-01-07 00:08:51 +0100

    can generate thumb polaroid

--- polaroid.py
+++ polaroid.py
@@ -6,29 +6,13 @@ Thanks to https://raw.githubusercontent.com/thegaragelab/pythonutils/master/pola
 import sys
 import logging
 from os import path
+from types import SimpleNamespace
 
 from PIL import Image, ImageDraw
 
-# Real kodak mini proportion
-POLAROID_TOP = 140
-POLAROID_BOTTOM = 280
-POLAROID_LEFT = 70
-POLAROID_RIGHT = POLAROID_LEFT
-COLOR_FRAME = (255, 255, 255)
-
-# POLAROID_TOP = 100
-# POLAROID_BOTTOM = 200
-# POLAROID_LEFT = 70
-# POLAROID_RIGHT = POLAROID_LEFT
-# COLOR_FRAME = (255, 255, 255)
-
 BORDER_SIZE = 1
-BORDER_COLOR = (90, 90, 90)
-
-HD_WIDTH = 760
-HD_HEIGHT = 1000
-MINI_WIDTH = int(HD_WIDTH / 2)
-MINI_HEIGHT = int(HD_HEIGHT / 2)
+BORDER_COLOR = (100, 100, 100)
+COLOR_FRAME = (255, 255, 255)
 
 logger = logging.getLogger()
 
@@ -44,29 +28,37 @@ def setup_logger():
     logger.addHandler(console)
 
 
-def crop_center(image, target_width, target_height):
+def crop_center(image, measures):
     """
     crop at the center
     return a new Image instance
     """
     width, height = image.size
-    if width == target_width:
-        delta = (height - target_height) / 2
+    if width == measures.width:
+        delta = (height - measures.height) / 2
         box = (0, delta, width, height - delta)
     else:
-        delta = (width - target_width) / 2
+        delta = (width - measures.width) / 2
         box = (delta, 0, width - delta, height)
 
     return image.crop(box)
 
 
-def add_frame(image):
+def add_frame(image, measures):
     """Adds the polaroid frame around the image"""
     frame = Image.new(
         "RGB",
         (
-            BORDER_SIZE + POLAROID_LEFT + HD_WIDTH + POLAROID_RIGHT + BORDER_SIZE,
-            BORDER_SIZE + POLAROID_TOP + HD_HEIGHT + POLAROID_BOTTOM + BORDER_SIZE,
+            BORDER_SIZE
+            + measures.frame.left
+            + measures.width
+            + measures.frame.right
+            + BORDER_SIZE,
+            BORDER_SIZE
+            + measures.frame.top
+            + measures.height
+            + measures.frame.bottom
+            + BORDER_SIZE,
         ),
         BORDER_COLOR,
     )
@@ -84,7 +76,7 @@ def add_frame(image):
     )
 
     # Add the source image
-    frame.paste(image, (POLAROID_LEFT, POLAROID_TOP))
+    frame.paste(image, (measures.frame.left, measures.frame.top))
 
     return frame
 
@@ -97,17 +89,12 @@ def build_target(filepath, folder):
     return path.join(path.dirname(path.realpath(__file__)), folder, name + ".jpg")
 
 
-def can_create_mini_polaroid(image):
+def can_create_polaroid(image, measures):
     width, height = image.size
-    return width >= MINI_WIDTH and height > MINI_HEIGHT
+    return width >= measures.width and height > measures.height
 
 
-def can_create_hd_polaroid(image):
-    width, height = image.size
-    return width >= HD_WIDTH and height > HD_HEIGHT
-
-
-def rescale_keep_one_side(image, keep_width, keep_height):
+def rescale_keep_one_side(image, measures):
     """
     rescale but force one side.
     if in landscape -> keep height
@@ -116,21 +103,22 @@ def rescale_keep_one_side(image, keep_width, keep_height):
     width, height = image.size
 
     if width > height:
-        resized_height = keep_height
-        resized_width = int(round((keep_height / float(height)) * width))
+        resized_height = measures.height
+        resized_width = int(round((measures.height / float(height)) * width))
     else:
-        resized_width = keep_width
-        resized_height = int(round((keep_width / float(width)) * height))
+        resized_width = measures.width
+        resized_height = int(round((measures.width / float(width)) * height))
 
     return image.resize(
         (resized_width, resized_height), resample=Image.LANCZOS, reducing_gap=3
     )
 
 
-def create_hd_polaroid(image):
-    scaled_image = rescale_keep_one_side(image, HD_WIDTH, HD_HEIGHT)
-    croped_image = crop_center(scaled_image, HD_WIDTH, HD_HEIGHT)
-    frammed_image = add_frame(croped_image)
+def create_polaroid(image, measures):
+    scaled_image = rescale_keep_one_side(image, measures)
+    croped_image = crop_center(scaled_image, measures)
+    frammed_image = add_frame(croped_image, measures)
+    logger.info("use %s", measures)
     logger.info("  image size is %s", image.size)
     logger.info(" scaled size is %s", scaled_image.size)
     logger.info("cropped size is %s", croped_image.size)
@@ -150,30 +138,44 @@ def main(args):
     filepath = args[0]
     source_image = Image.open(filepath)
 
-    if can_create_hd_polaroid(source_image):
-        hd_image = create_hd_polaroid(source_image)
+    # other proportion
+    # hd_measures = SimpleNamespace(
+    #     width=760,
+    #     height=1000,
+    #     # frame=SimpleNamespace(top=100, bottom=200, left=70, right=70),
+    # )
+    hd_measures = SimpleNamespace(
+        width=760,
+        height=1000,
+        frame=SimpleNamespace(top=140, bottom=280, left=70, right=70),
+    )
+    mini_measures = SimpleNamespace(
+        width=380,
+        height=500,
+        frame=SimpleNamespace(top=70, bottom=140, left=35, right=35),
+    )
+    if can_create_polaroid(source_image, hd_measures):
+        hd_image = create_polaroid(source_image, hd_measures)
         hd_path = save_to(hd_image, filepath, "polaroid-hd")
         logger.info("saved HD to %s", hd_path)
     else:
         logger.warning(
-            "%s : %s is too small to have a polaroid of : %s X %s",
+            "%s : %s is too small to have a polaroid of : %s",
             filepath,
             source_image.size,
-            HD_WIDTH,
-            HD_HEIGHT,
+            hd_measures,
         )
 
-    if can_create_mini_polaroid(source_image):
-        mini_image = rescale_keep_one_side(source_image, MINI_WIDTH, MINI_HEIGHT)
+    if can_create_polaroid(source_image, mini_measures):
+        mini_image = create_polaroid(source_image, mini_measures)
         mini_path = save_to(mini_image, filepath, "polaroid-mini")
         logger.info("saved mini to %s", mini_path)
     else:
         logger.warning(
-            "%s : %s is too small to have a polaroid of : %s X %s",
+            "%s : %s is too small to have a polaroid of : %s",
             filepath,
             source_image.size,
-            MINI_WIDTH,
-            MINI_HEIGHT,
+            mini_measures,
         )
 
 
